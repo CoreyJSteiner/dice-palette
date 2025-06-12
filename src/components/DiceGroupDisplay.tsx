2f06ed0e4b330ba73811d5d740b2e08af5fe9fe7
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-06-12 12:40:53 -0400

    css updates

--- src/components/DiceGroupDisplay.tsx
+++ src/components/DiceGroupDisplay.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect } from 'react';
+import { useState, useRef } from 'react';
 import { useDroppable } from "@dnd-kit/core"
 import DiceGroup from "./DiceGroup"
 import DieDisplay from "./DieDisplay"
