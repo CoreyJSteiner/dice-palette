99ce2b1092d2b2ed61bb7e469787bd1b83294051
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-06-13 12:48:06 -0400

    removes hovering requirement for addGroup and moves to DieOptions component

--- src/components/DieButton.tsx
+++ src/components/DieButton.tsx
@@ -1,54 +1,17 @@
-import { useState, useEffect, useRef } from "react"
 import DieImage from "./DieImage"
 
 type DieButtonProps = {
     imageName?: string
     dieSides: number
+    addGroup: string | null | undefined
     onAddDie: (dieSides: number, groupKey: string | null | undefined) => void
 }
 
-const DieButton: React.FC<DieButtonProps> = ({ imageName = 'd20', dieSides = 20, onAddDie }) => {
-    const [isHovering, setIsHovering] = useState<boolean>(false)
-    const addGroup = useRef<string | null | undefined>(null)
-
-    // Effects
-    useEffect(() => {
-        const handleKeyboardDownEvent = (e: KeyboardEvent) => {
-            const handleGroupAdd = () => {
-                if (addGroup.current) return
-                addGroup.current = crypto.randomUUID()
-            }
-
-            if (!isHovering) return
-            e.preventDefault()
-            if (e.code === 'Space') handleGroupAdd()
-        }
-
-        const handleKeyboardUpEvent = (e: KeyboardEvent) => {
-            const handleClearAddGroup = () => {
-                addGroup.current = null
-            }
-
-            if (!isHovering) return
-            console.log(e.code)
-            e.preventDefault()
-            if (e.code === 'Space') handleClearAddGroup()
-        }
-
-        window.addEventListener('keydown', handleKeyboardDownEvent)
-        window.addEventListener('keyup', handleKeyboardUpEvent)
-        return () => {
-            window.removeEventListener('keydown', handleKeyboardDownEvent)
-            window.removeEventListener('keyup', handleKeyboardUpEvent)
-        }
-    }, [isHovering])
-
+const DieButton: React.FC<DieButtonProps> = ({ imageName = 'd20', dieSides = 20, onAddDie, addGroup }) => {
     return (
         <button
             className='die-button'
-            onClick={() => onAddDie(dieSides, addGroup.current)}
-            onMouseEnter={() => setIsHovering(true)}
-            onMouseLeave={() => setIsHovering(false)}
+            onClick={() => onAddDie(dieSides, addGroup)}
         >
             <DieImage imageName={imageName} />
         </button>
--- src/components/DieOptions.tsx
+++ src/components/DieOptions.tsx
@@ -1,19 +1,54 @@
 import type React from "react"
 import DieButton from "./DieButton"
+import { useState, useEffect } from "react"
 
 type DieOptionsProps = {
     onAddDie: (dieSides: number, groupKey: string | null | undefined) => void
 }
 
 const DieOptions: React.FC<DieOptionsProps> = ({ onAddDie }) => {
+    const [addGroup, setAddGroup] = useState<string | null | undefined>(null)
+
+    // Effects
+    useEffect(() => {
+        const handleKeyboardDownEvent = (e: KeyboardEvent) => {
+            const handleGroupAdd = () => {
+                if (addGroup) return
+                setAddGroup(crypto.randomUUID())
+            }
+
+            e.preventDefault()
+            if (e.code === 'Space') handleGroupAdd()
+        }
+
+        const handleKeyboardUpEvent = (e: KeyboardEvent) => {
+            const handleClearAddGroup = () => {
+                setAddGroup(null)
+            }
+
+            console.log(e.code)
+            e.preventDefault()
+            if (e.code === 'Space') handleClearAddGroup()
+        }
+
+        window.addEventListener('keydown', handleKeyboardDownEvent)
+        window.addEventListener('keyup', handleKeyboardUpEvent)
+        return () => {
+            window.removeEventListener('keydown', handleKeyboardDownEvent)
+            window.removeEventListener('keyup', handleKeyboardUpEvent)
+        }
+    }, [addGroup])
+
     return (
-        <div className="die-options">
-            <DieButton imageName="d4" dieSides={4} onAddDie={onAddDie} />
-            <DieButton imageName="d6" dieSides={6} onAddDie={onAddDie} />
-            <DieButton imageName="d8" dieSides={8} onAddDie={onAddDie} />
-            <DieButton imageName="d10" dieSides={10} onAddDie={onAddDie} />
-            <DieButton imageName="d12" dieSides={12} onAddDie={onAddDie} />
-            <DieButton imageName="d20" dieSides={20} onAddDie={onAddDie} />
+        <div
+            className="die-options"
+        >
+            <DieButton imageName="d4" dieSides={4} onAddDie={onAddDie} addGroup={addGroup} />
+            <DieButton imageName="d6" dieSides={6} onAddDie={onAddDie} addGroup={addGroup} />
+            <DieButton imageName="d8" dieSides={8} onAddDie={onAddDie} addGroup={addGroup} />
+            <DieButton imageName="d10" dieSides={10} onAddDie={onAddDie} addGroup={addGroup} />
+            <DieButton imageName="d12" dieSides={12} onAddDie={onAddDie} addGroup={addGroup} />
+            <DieButton imageName="d20" dieSides={20} onAddDie={onAddDie} addGroup={addGroup} />
         </div>
     )
 }
