1c76dcc7fa39002fae1f5c65e267a14cd5026ee1
Corey Steiner <corey.joseph.steiner@gmail.com>
2025-06-28 19:04:55 -0400

    attempted fix for setting the vh in mobile

--- src/components/DiePalette.tsx
+++ src/components/DiePalette.tsx
@@ -33,6 +33,17 @@ const DiePallete: React.FC = () => {
         };
     }, []);
 
+    useEffect(() => {
+        const setVh = () => {
+            const vh = window.innerHeight * 0.01
+            document.documentElement.style.setProperty('--vh', `${vh}px`)
+        }
+
+        setVh()
+        window.addEventListener('resize', setVh)
+
+        return () => window.removeEventListener('resize', setVh)
+    }, [])
 
     useEffect(() => {
         if (heldKeysRef.current.has('Space')) return
